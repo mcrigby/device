726a94826e16b056a98f59b1c6afa9e74bf492fc
Martin Cutillo-Rigby <70958005+mcrigby@users.noreply.github.com>
2023-05-18 14:15:10 +0100

    add register

---
+++ src/Register.cs
@@ -0,0 +1,29 @@
+﻿namespace CutilloRigby.Device;
+
+public abstract class Register
+{
+    protected byte _value;
+
+    protected readonly Func<byte> _getValue;
+    protected readonly Action<byte> _onWriteValue;
+
+    protected Register(Func<byte> getValue, Action<byte> onWriteValue)
+    {
+        _getValue = getValue;
+        _onWriteValue = onWriteValue;
+    }
+
+    protected Func<byte> GetReadValue(byte readMask)
+    {
+        return () => (byte)(_value & readMask);
+    }
+    protected Action<byte> GetWriteValue(byte writeMask)
+    {
+        return value => {
+            _value &= writeMask;
+            _value |= value;
+
+            _onWriteValue(_value);
+        };
+    }
+}
